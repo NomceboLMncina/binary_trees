79ef3df75d4e3c458668708267a44b35a2836a10
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 10:58:31 -0800

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,11 +4,11 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Return: Height of the tree (number of edges on the longest path from the root)
+ * Return: Height of the tree (num of edges on the longest path from the root)
  *         0 if the tree is NULL
  *
- * Description: This function calculates the height of a binary tree by recursively
- *              determining the height of its left and right subtrees, and returning
+ * Description: This functn calc the height of a binary tree by recursively
+ *              determining the h of its left and right subtrees, and returning
  *              the greater of the two heights, plus 1 for the current node.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -19,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 	return (0);
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;	
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
 }
