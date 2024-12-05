e8a9621ae3d36b9e7d57aa3c3a00166e176b9cae
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 09:46:01 -0800

    Is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Checks if a node is the root of a binary tree
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if the node is the root (has no parent),
+ *         0 if the node is not the root or is NULL.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    return ((!node || node->parent) ? 0 : 1);
+}
