2999b200e2ce68a369c58fda7045ef4645a6ad26
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 09:38:23 -0800

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Description: Recursively deletes all nodes of a binary tree,
+ *              starting from the leaves and moving up to the root.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (!tree)
+        return;
+
+    binary_tree_delete(tree->right);
+    binary_tree_delete(tree->left);
+    free(tree);
+}
