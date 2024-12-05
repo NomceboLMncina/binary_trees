44766017a98293d6167babd2dcbf26f89fc8bbf9
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 13:30:55 -0800

    Rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,16 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - Performs a right-rotation on a binary tree
- * @tree: A pointer to the root node of the tree to rotate.
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
+ * @tree: a pointer to the root node of the tree to check
  *
- * A right rotation involves shifting the root to the right, making its left
- * child the new root, and the original root becomes the right child of the
- * new root. This operation is commonly used to maintain balance in binary
- * search trees, such as AVL trees or Red-Black trees.
- *
- * Return: Pointer to the new root node of the tree after rotation,
- *         NULL upon failure (if the tree is NULL or does not have a left child).
+ * Return: Pointer to the new root node of the tree once rotated
+ *         NULL upon failure
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
@@ -18,12 +13,10 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 	if (!tree || !tree->left)
 		return (NULL);
-
 	tmp = tree;
 	parent = tree->parent;
 	tree = tree->left;
 	tree->parent = NULL;
-
 	if (tree->right)
 	{
 		tmp->left = tree->right;
@@ -31,14 +24,10 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	}
 	else
 		tmp->left = NULL;
-
 	tmp->parent = tree;
 	tree->right = tmp;
-
 	if (parent)
 		parent->left = tree;
-
 	tree->parent = parent;
-
 	return (tree);
 }
