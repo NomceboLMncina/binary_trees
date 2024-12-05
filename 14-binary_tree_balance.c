68ba77791dfe590d10eec14b2afa695a4276d5a2
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 12:34:04 -0800

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-	return (0);
+	int height_l, height_r;
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+	return (height_l - height_r);
 }
 
 /**
@@ -28,7 +33,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_r = 0;
 
 	if (!tree)
-	return (0);
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
