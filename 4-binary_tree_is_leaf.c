1d20536b0dbb975494270612311cb4009fef911a
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-12-05 09:41:08 -0800

    Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf in a binary tree
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if the node is a leaf (has no children),
+ *         0 if the node has children or is NULL.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    return ((!node || node->left || node->right) ? 0 : 1);
+}
